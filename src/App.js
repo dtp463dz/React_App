7efa1b973e0b69acd4d904d49ce205c701fb4abb
PhucDT <dinhthaiphuc463@gmail.com>
2025-02-26 23:30:06 +0700

    hpbd_262_22

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HappyBirthday from './test/hpbd';
 
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <div>Count = {count}</div>
+          <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+          <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+          <nav>
+            <Link to="/">Home</Link>|<Link to="/birthday">Happy Birthday</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/birthday" element={<HappyBirthday />} />
+          <Route path="/" element={<div>Welcome to the Home Page</div>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/test/hpbd.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HappyBirthday = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px', position: 'relative' }}>
+            <h1>Happy Birthday!</h1>
+            <p>Congratulations on turning 22 years old!</p>
+        </div>
+    );
+};
+
+export default HappyBirthday;
