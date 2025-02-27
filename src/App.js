d3d40600ee50ee532860f4746bad1522183e9423
PhucDT <dinhthaiphuc463@gmail.com>
2025-02-27 16:20:40 +0700

    16Component

--- src/App.js
+++ src/App.js
@@ -3,32 +3,37 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HappyBirthday from './test/hpbd';
+import HappyBirthday from './test/Happy';
+import MyComponent from './components/MyComponent';
 
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <div>Count = {count}</div>
-          <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-          <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-          <nav>
-            <Link to="/">Home</Link>|<Link to="/birthday">Happy Birthday</Link>
-          </nav>
-        </header>
-        <Routes>
-          <Route path="/birthday" element={<HappyBirthday />} />
-          <Route path="/" element={<div>Welcome to the Home Page</div>} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      Hello World With ReactJS
+      <MyComponent />
+    </div>
+    // <Router>
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <div>Count = {count}</div>
+    //       <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //       <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //       <nav>
+    //         <Link to="/">Home</Link>|<Link to="/birthday">Happy Birthday</Link>
+    //       </nav>
+    //     </header>
+    //     <Routes>
+    //       <Route path="/birthday" element={<HappyBirthday />} />
+    //       <Route path="/" element={<div>Welcome to the Home Page</div>} />
+    //     </Routes>
+    //   </div>
+    // </Router>
   );
 }
 
---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            <div>Hello MyComponent
+                {Math.random()}
+            </div>
+
+        );
+    }
+
+}
+
+export default MyComponent;
