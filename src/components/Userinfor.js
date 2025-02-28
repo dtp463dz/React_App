d2dd965655815bb5b0afdee27b520e44746da47a
PhucDT <dinhthaiphuc463@gmail.com>
2025-02-28 20:40:09 +0700

    22NestingComponent

---
+++ src/components/Userinfor.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+class UserInfor extends React.Component {
+    state = {
+        name: 'Dean',
+        address: 'Vinh Phuc',
+        age: 22,
+    }
+
+    handleClick(event) {
+        // console.log(" My name is", this.state.name, "Adress ", this.state.address);
+        console.log(">> click me")
+
+        //thay doi state 
+        // merge state chỉ ở react class
+        this.setState({
+            name: 'Ngoc Anh',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+    }
+
+    handleOnMoverOver(event) {
+        // console.log(event)
+    }
+
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+        console.log(">>> check form input:  ", event.target.value)
+    }
+
+    handleOnChangeAge = (event) => {
+        this.setState({
+            age: event.target.value
+        })
+        console.log(">>> check form input:  ", event.target.value)
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault(); // hàm k load lại website
+        console.log('>> check submit: ', this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                My name is {this.state.name} and I'm {this.state.age} years old.
+                {/* <button onClick={(event) => { this.handleClick(event) }}>Click me</button> */}
+                {/* <button onMouseOver={this.handleOnMoverOver}>Over</button> */}
+                <form onSubmit={(event) => this.handleSubmit(event)}>
+                    <label>Your name: </label>
+                    <input
+                        value={this.state.name}
+                        type="text"
+                        onChange={(event) => this.handleOnChangeInput(event)}
+                    />
+
+                    <label>Your age: </label>
+                    <input
+                        value={this.state.age}
+                        type="text"
+                        onChange={(event) => this.handleOnChangeAge(event)}
+                    />
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default UserInfor;
