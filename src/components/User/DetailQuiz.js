29956b592712a5f7ef0aaf3dee2b8b74f0921625
PhucDT <dinhthaiphuc463@gmail.com>
2025-03-22 23:40:34 +0700

    83Xu ly data phia FE usinglodash

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDataQuiz } from '../../services/apiService';
+import _ from 'lodash'; // sử dụng lodash để check obj có rỗng hay ko
 
 const DetailQuiz = (props) => {
     const params = useParams(); // lấy tham số trên đường link URL
@@ -14,6 +15,27 @@ const DetailQuiz = (props) => {
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
         console.log('check question: ', res)
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                .groupBy("id")
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers)
+                        console.log('item answers: ', item.answers)
+                    })   // forEach lặp từng đối tượng 1
+                    console.log('value: ', value, 'key: ', key)
+                    return { questionId: key, answers: answers, questionDescription, image }
+                })
+                .value()
+            console.log(data)
+        }
     }
 
 
